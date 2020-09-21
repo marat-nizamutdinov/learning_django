c2c97114ca2e04d0094166e275c31cb67f73c1af
Marat Nizamutdinov <m.nizamutdinov@just-work.org>
2020-09-21 10:06:47 +0300

    updateted readthedocs files

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,9 @@ release = '0.0.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -53,4 +55,3 @@ html_theme = 'classic'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 master_doc = 'index'
-index_doc = 'index'
